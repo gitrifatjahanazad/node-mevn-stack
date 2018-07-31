07cf11322175803e53a2696b5325e647fbf25f6d
Rifat Azad <rifat.jahan@bs-23.net>
2018-07-31 16:21:56 +0600

    initialization

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vue JS MEVN stack exploring
+
+## Mongo Explorings
+[Mongo explorings]()
+## Express Explorations
+[Express explorings](adf)
+## Vue Explorations
+[Vue explorings](VUE_js_very_basic/vue_js_findings.md)
+## Node Explorations
+[Node explorings](adf)
---
+++ VUE_js_very_basic/vuecode.js
@@ -0,0 +1,37 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
+
+  var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+  })
+
+  var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
+    }
+  })
+
+
+  Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }} <input v-model="todo.text" /></li>'
+  })
+  
+  var app7 = new Vue({
+    el: '#app-7',
+    data: {
+      groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
+  })
